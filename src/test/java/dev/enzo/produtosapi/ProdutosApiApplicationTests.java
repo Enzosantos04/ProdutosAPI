b119e0a72057ab0e5544f8547494aa1369856f7d
enzosdev <enzohfs@gmail.com>
2025-06-16 20:00:48 -0300

    first commit

---
+++ src/test/java/dev/enzo/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.enzo.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
